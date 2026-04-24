da6f34f5a1f1e8b4a37f66db71d780e886e244e7
saeed shabibi <saeed.a.shabibi@gmail.com>
2026-04-24 22:15:07 +0530

    changes in server.js file for connecting mongo and mongosxpress port

--- server.js
+++ server.js
@@ -1,42 +1,66 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const MongoClient = require("mongodb").MongoClient;
 
 const PORT = 5050;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MONGO_URL = "mongodb://admin:qwerty@localhost:27017";
+const MONGO_URL =
+  "mongodb://admin:qwerty@mongo:27017/?authSource=admin";
+
 const client = new MongoClient(MONGO_URL);
 
-//GET all users
+let db;
+
+// Connect ONCE when server starts
+async function connectDB() {
+  try {
+    await client.connect();
+
+    console.log("Connected successfully to MongoDB");
+
+    db = client.db("apnacollege-db");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+connectDB();
+
+// GET all users
 app.get("/getUsers", async (req, res) => {
-    await client.connect(URL);
-    console.log('Connected successfully to server');
+  try {
+    const data = await db.collection("users").find({}).toArray();
 
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').find({}).toArray();
-    
-    client.close();
     res.send(data);
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).send("Error fetching users");
+  }
 });
 
-//POST new user
+// POST new user
 app.post("/addUser", async (req, res) => {
+  try {
     const userObj = req.body;
-    console.log(req.body);
-    await client.connect(URL);
-    console.log('Connected successfully to server');
 
-    const db = client.db("apnacollege-db");
-    const data = await db.collection('users').insertOne(userObj);
-    console.log(data);
-    console.log("data inserted in DB");
-    client.close();
+    console.log(userObj);
+
+    const result = await db.collection("users").insertOne(userObj);
+
+    console.log(result);
+
+    res.send("User added successfully");
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).send("Error adding user");
+  }
 });
-
 
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
